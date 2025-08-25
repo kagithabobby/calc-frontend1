8240475593adcbb2f84f15a9d07177450dfb9fd3
kagithabobby <bobbykagitha1980@gmail.com>
2025-08-25 09:56:53 +0530

    api.js

--- src/api.js
+++ src/api.js
@@ -1,4 +1,4 @@
-export const BASEURL = "http://localhost:8081/";
+export const BASEURL = "http://localhost:8080/calc-frontend1/";
 export function callApi(reqmethod, url, data, responseHandler)
 {
     let options = "";
